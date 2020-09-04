8b326ff370032de0fb7fed35b5c4c13e419436d4
Jasleen <jasleenk1902@gmail.com>
2020-09-04 16:04:31 +0530

    Adding Q5

--- A2/Q5.java
+++ A2/Q5.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Ques5_ass2{
+public class Q5{
     public static void main(String[] args){
         Scanner obj = new Scanner(System.in);
         System.out.print("Enter maximum limit for universe starting from 0: ");
